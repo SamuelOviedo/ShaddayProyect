51806a46fd1a42de18e090c201b7c2a09ccc9f8a
Samuel Oviedo <isaaccoviedo@gmail.com>
2024-04-05 22:01:28 -0600

    primera vista

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:poyectoshadday/widgets/navigationBar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: [
+        NavigationBarWidget(),
+      ],),
+    );
   }
 }
--- lib/widgets/navigationBar/navigation_bar.dart
+++ lib/widgets/navigationBar/navigation_bar.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 
-class NavigationBar extends StatelessWidget {
-  const NavigationBar({super.key});
+class NavigationBarWidget extends StatelessWidget {
+  const NavigationBarWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100,
       child: Row(
-        children: [
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
           SizedBox(
             height: 80,
             width: 150,
-            child: Image.asset('assets/logo.png'),
+            child: Image.asset('assets/images/LogoNuevoShadday.png'),
           ),
-          Row(
+          const Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              _NavBarItem('Samuel'),
-              
+              _NavBarItem(title: 'Episodes'),
+              SizedBox(
+                width: 60,
+              ),
+              _NavBarItem(title: 'About')
             ],
           ),
         ],
